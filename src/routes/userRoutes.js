e6c3bc0172d783324b97a2c2cceabd8d3e78773b
LOGO1012 <ekthf988173@gmail.com>
2025-03-17 15:58:50 +0900

    feat: user star fn add

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 // routes/userRoutes.js
 import express from "express";
-import { getUserInfo } from "../controllers/userController.js";
+import {getUserInfo, rateUserController} from "../controllers/userController.js";
 
 const router = express.Router();
 
 // 사용자 정보 가져오기
 router.get("/user/:userId", getUserInfo);
+router.post("/user/:userId/rate", rateUserController);
 
 
 export default router;
