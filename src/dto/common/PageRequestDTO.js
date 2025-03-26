d21f280e09d688299146678a25b03b8ac0e5d34c
LOGO1012 <ekthf988173@gmail.com>
2025-03-26 17:56:02 +0900

    feat: qnaStatus add

--- src/dto/common/PageRequestDTO.js
+++ src/dto/common/PageRequestDTO.js
@@ -1,10 +1,11 @@
 
 class PageRequestDTO {
 
-    constructor(page = 1, size = 10) {
+    constructor(page = 1, size = 10, qnaStatus = null) {
 
         this.page = page;
         this.size = size;
+        this.qnaStatus = qnaStatus;
     }
 }
 
