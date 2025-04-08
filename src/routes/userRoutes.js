5be8cedc733f86adeef9bf498a68d375a3be1681
Jeongseok1 <wjdtjr46179702@gmail.com>
2025-04-08 17:50:42 +0900

    마이페이지, 간략프로필

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 // src/routes/userRoutes.js
 import express from 'express';                     // Express 모듈 불러오기
 import { registerUserProfile } from '../controllers/userProfileController.js'; // 회원가입 컨트롤러 함수 불러오기
-import {getUserInfo, rateUserController} from "../controllers/userController.js";
+import {getUserInfo, rateUserController, updateUserProfile } from "../controllers/userController.js";
 
 const router = express.Router();                   // Express 라우터 인스턴스 생성
 
@@ -10,7 +10,10 @@ router.post('/register', registerUserProfile);
 
 // 사용자 정보 가져오기
 router.get("/:userId", getUserInfo);
+// 유저 별점 업데이트 엔드포인트
 router.post("/:userId/rate", rateUserController);
+// 유저 프로필 업데이트 엔드포인트 (PATCH 메소드)
+router.patch("/:userId", updateUserProfile);
 
 
 export default router;
