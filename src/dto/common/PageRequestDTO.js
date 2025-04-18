7ed407bb9604d53c8d3dffb1a120f022804586af
LOGO1012 <ekthf988173@gmail.com>
2025-04-18 16:07:43 +0900

    chore: search type add

--- src/dto/common/PageRequestDTO.js
+++ src/dto/common/PageRequestDTO.js
@@ -1,9 +1,11 @@
 class PageRequestDTO {
-    constructor(page = 1, size = 10, qnaStatus = '', keyword = '') {
+    constructor(page = 1, size = 10, qnaStatus = '', keyword = '', searchType   = 'both') {
         this.page = page;
         this.size = size;
         this.qnaStatus = qnaStatus;
         this.keyword = keyword;
+        this.searchType = searchType;
+
     }
 }
 export default PageRequestDTO;
