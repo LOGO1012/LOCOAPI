ba9bd67e67955963fa598fe1e969ae97662ca187
Jeongseok1 <wjdtjr46179702@gmail.com>
2025-04-18 14:28:12 +0900

    패이징 DTO

--- src/dto/common/PageRequestDTO.js
+++ src/dto/common/PageRequestDTO.js
@@ -1,11 +1,12 @@
 
 class PageRequestDTO {
 
-    constructor(page = 1, size = 10, qnaStatus = null) {
+    constructor(page = 1, size = 10, qnaStatus = null, keyword = '') {
 
         this.page = page;
         this.size = size;
         this.qnaStatus = qnaStatus;
+        this.keyword = keyword;
     }
 }
 
