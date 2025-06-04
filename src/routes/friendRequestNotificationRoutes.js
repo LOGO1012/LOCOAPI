c0fd60863cd51457b52ac7c7afab8953ce25ff45
LOGO1012 <ekthf988173@gmail.com>
2025-06-04 14:42:31 +0900

    chore: unnecessary delete / edit

--- src/routes/friendRequestNotificationRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/friendRequestNotificationRoutes.js
-import express from 'express';
-import * as ctrl from '../controllers/friendRequestNotificationController.js';
-const router = express.Router();
-
-router.get('/:userId', ctrl.getNotifs);
-router.put('/:id/read',    ctrl.markRead);
-
-export default router;
